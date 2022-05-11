477e03eafa9dfdfe8f116808026b155799af625d
April Archibald <aprilsclearskies@gmail.com>
2022-05-11 12:21:12 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+
+const [poke, setPoke] = useState([])
+
+  const getPokemon = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+      .then(response => {
+        return response.json();
+      }).then(response => {
+      console.log(response.results);
+      setPoke(response.results)
+      }).catch(err=>{
+      console.log(err);
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getPokemon}>Fetch Pokemon</button>
+      {/* poke: {JSON.stringify(poke)} */}
+      <ul>
+          {poke.map( (onePoke, idx) =>{
+            return <li key={idx}>{onePoke.name}</li> 
+          })
+          }
+      </ul>
+
     </div>
   );
 }
